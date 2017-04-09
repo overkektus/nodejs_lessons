4b20acc24e4a84d1152a8fbcd8e1647b105a93a6
overkektus <egor.piskunou@gmail.com>
2017-04-09 11:42:37 +0300

    delete, update and put data

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express');
+var express     = require('express'),
+    bodyParser  = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
 
 var artists = [
    {
@@ -19,11 +23,11 @@ var artists = [
 
 app.get('/', function(req, res) {
    res.send('Hello API');
-});
+})
 
 app.get('/artists', function(req, res) {
    res.send(artists);
-});
+})
 
 app.get('/artists/:id', function (req, res) {
    var artist = artists.find(function (artist) {
@@ -32,6 +36,30 @@ app.get('/artists/:id', function (req, res) {
    res.send(artist);
 })
 
+app.post('/artists', function (req, res) {
+   var artist = {
+      id: Date.now(),
+      name: req.body.name
+   };
+   artists.push(artist);
+   res.send(artist);
+})
+
+app.put('/artists/:id', function (req, res) {
+   var artist = artists.find(function (artist) {
+      return artist.id === Number(req.params.id);
+   });
+   artist.name = req.body.name;
+   res.sendStatus(200);
+})
+
+app.delete('/artists/:id', function (req, res){
+   artists = artists.filter(function (artist) {
+      return artist.id !== Number(req.params.id);
+   })
+   res.sendStatus(200);
+})
+
 app.listen(3012, function() {
    console.log('API app started');
-});
+})
