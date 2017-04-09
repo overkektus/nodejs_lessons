073056f242874660796e4d528cbe4511b793915e
overkektus <egor.piskunou@gmail.com>
2017-04-09 14:53:17 +0300

    use mongodb

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express     = require('express'),
-    bodyParser  = require('body-parser');
+    bodyParser  = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient,
+    ObjectID    = require('mongodb').ObjectID;
 
 var app = express();
+var db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
@@ -26,23 +29,38 @@ app.get('/', function(req, res) {
 })
 
 app.get('/artists', function(req, res) {
-   res.send(artists);
+   db.collection('artists').find().toArray(function(err, docs) {
+      if (err) {
+         console.log(err);
+         return res.sendStatus(500);
+      }
+      res.send(docs);
+   })
+   //res.send(artists);
 })
 
 app.get('/artists/:id', function (req, res) {
-   var artist = artists.find(function (artist) {
-      return artist.id === Number(req.params.id);
-   });
-   res.send(artist);
+   db.collection('artists').findOne({ _id: ObjectID(req.params.id) }, function(err, doc) {
+      if(err){
+         console.log(err);
+         return res.sendSatus(500);
+      }
+      res.send(doc);
+   })
 })
 
 app.post('/artists', function (req, res) {
    var artist = {
-      id: Date.now(),
       name: req.body.name
    };
-   artists.push(artist);
-   res.send(artist);
+   db.collection('artists').insert(artist, function(err, result){
+      if (err) {
+         console.log(err);
+         return res.sendStatus(500);
+      }
+      res.send(artist);
+   })
+   //res.send(artist);
 })
 
 app.put('/artists/:id', function (req, res) {
@@ -60,6 +78,12 @@ app.delete('/artists/:id', function (req, res){
    res.sendStatus(200);
 })
 
-app.listen(3012, function() {
-   console.log('API app started');
+MongoClient.connect('mongodb://localhost:27017/myapi', function (err, database){
+   if (err) {
+      return console.log(err);
+   }
+   db = database;
+   app.listen(3012, function() {
+      console.log('API app started');
+   })
 })
